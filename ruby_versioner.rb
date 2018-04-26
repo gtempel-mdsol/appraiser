1779281af95c57bef360a08b8ed9cc58b56625c0
george tempel <gtempel@mdsol.com>
2018-04-26 15:35:55 -0400

    streamlined << method

--- ruby_versioner.rb
+++ ruby_versioner.rb
@@ -5,15 +5,13 @@ class RubyVersioner
   end
 
   def <<(gem_info)
-    if gem_info&.valid?
-      add_gem_info  gem_name: gem_info.name,
-                    gem_version: gem_info.current_version.gem,
-                    ruby_version: gem_info.current_version.ruby
+    add_gem_info  gem_name: gem_info.name,
+                  gem_version: gem_info.current_version.gem,
+                  ruby_version: gem_info.current_version.ruby if gem_info&.valid?
 
-      add_gem_info  gem_name: gem_info.name,
-                    gem_version: gem_info.newest_version.gem,
-                    ruby_version: gem_info.newest_version.ruby if gem_info.newer_version?
-    end
+    add_gem_info  gem_name: gem_info.name,
+                  gem_version: gem_info.newest_version.gem,
+                  ruby_version: gem_info.newest_version.ruby if gem_info&.valid? && gem_info&.newer_version?
   end
 
   def ruby_versions
